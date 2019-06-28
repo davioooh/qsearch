39f6907d8c05fa2f0d19c17b2d1080c33c4e5a4f
David Castelletti <d.castelletti@cogitoo.com>
2019-06-28 10:13:50 +0200

    Add SO API call to fetch access token

--- src/main/kotlin/com/davioooh/stackoverflow/api/ApiCalls.kt
+++ src/main/kotlin/com/davioooh/stackoverflow/api/ApiCalls.kt
@@ -1,13 +1,26 @@
 package com.davioooh.stackoverflow.api
 
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.jackson.responseObject
 
 class SoAuthApi(
     private val clientId: String,
     private val clientSecret: String,
     private val redirectUri: String
 ) {
-    fun fetchAccessToken(code: String): AccessTokenDetails? =
-        TODO("Da implementare")
+
+    fun fetchAccessToken(code: String): AccessTokenDetails =
+        Fuel.post(
+            "$AUTH_BASE/oauth/access_token/json", listOf(
+                "redirect_uri" to redirectUri,
+                "client_id" to clientId,
+                "client_secret" to clientSecret,
+                "code" to code
+            )
+        ).responseObject<AccessTokenDetails>().let {
+            val (_, rep, res) = it
+            res.get()
+        }
 
     companion object {
         const val AUTH_BASE = "https://stackoverflow.com/oauth"
