38708b9ed4e4cf60b21c2c58b004e48865bf06c0
David Castelletti <d.castelletti@cogitoo.com>
2019-07-05 17:32:27 +0200

    Refactor pagination utilities

---
+++ src/main/kotlin/com/davioooh/qsearch/model/Pagination.kt
@@ -0,0 +1,27 @@
+package com.davioooh.qsearch.model
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import kotlin.math.ceil
+
+data class PageableResult<T>(
+    @JsonProperty("items") val items: List<T>,
+    @JsonProperty("page") val page: Int,
+    @JsonProperty("page_size") val pageSize: Int,
+    @JsonProperty("total") val total: Int
+)
+
+fun <T> paginate(items: List<T>, page: Int, pageSize: Int): PageableResult<T>? {
+    require(page > 0) { "page must be greater than 0" }
+    val lastPage = calculateLastPage(items, pageSize)
+    require(page <= lastPage) { "page can't be greater than $lastPage" }
+    require(pageSize > 0) { "pageSize must be greater than 0" }
+
+    return PageableResult(
+        items.chunked(pageSize)[page - 1],
+        page,
+        pageSize,
+        items.size
+    )
+}
+
+fun <T> calculateLastPage(items: List<T>, pageSize: Int) = ceil(items.size / pageSize.toFloat()).toInt()
---
+++ src/test/kotlin/com/davioooh/qsearch/model/PaginationTest.kt
@@ -0,0 +1,72 @@
+package com.davioooh.qsearch.model
+
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+internal class PaginationTest {
+    private val itemsList = listOf("ab", "bc", "cd", "de", "ef", "fg", "gh", "hi")
+
+    @Nested
+    inner class PaginateFun {
+
+        @Test
+        fun `when page is less than 0 throws an exception`() {
+            Assertions.assertThatExceptionOfType(IllegalArgumentException::class.java)
+                .isThrownBy { paginate(listOf<String>(), -1, 25) }
+        }
+
+        @Test
+        fun `when page is 0 throws an exception`() {
+            Assertions.assertThatExceptionOfType(IllegalArgumentException::class.java)
+                .isThrownBy { paginate(listOf<String>(), 0, 25) }
+        }
+
+        @Test
+        fun `when pageSize is less than 0 throws an exception`() {
+            Assertions.assertThatExceptionOfType(IllegalArgumentException::class.java)
+                .isThrownBy { paginate(listOf<String>(), 1, -1) }
+        }
+
+        @Test
+        fun `when pageSize is 0 throws an exception`() {
+            Assertions.assertThatExceptionOfType(IllegalArgumentException::class.java)
+                .isThrownBy { paginate(listOf<String>(), 0, 0) }
+        }
+
+        @Test
+        fun `when required page is greater than last page throws an exception`() {
+            val expectedLastPage = 1
+
+            Assertions.assertThatExceptionOfType(IllegalArgumentException::class.java)
+                .isThrownBy { paginate(itemsList, 2, 10) }
+                .withMessage("page can't be greater than $expectedLastPage")
+        }
+
+        @Test
+        fun `returns page correctly`() {
+            val expectedResult = PageableResult(
+                listOf("de", "ef", "fg"),
+                2,
+                3,
+                8
+            )
+
+            val result = paginate(itemsList, 2, 3)
+
+            assertThat(result).isNotNull()
+            if (result != null) {
+                assertThat(result).isEqualTo(expectedResult)
+            }
+        }
+    }
+
+    @Nested
+    inner class CalculateLastPageFun {
+        @Test
+        fun `calculate last page correctly`() {
+            assertThat(calculateLastPage(itemsList, 3)).isEqualTo(3)
+        }
+    }
+}
