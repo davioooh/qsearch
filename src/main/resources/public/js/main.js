e23e59c65e2fdc471e489d90905c8dc9d4970702
David Castelletti <d.castelletti@cogitoo.com>
2019-07-31 17:06:31 +0200

    Add support to sorting criteria + sorting direction

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -14,24 +14,38 @@ var app = new Vue({
     }
   },
   mounted () {
-    axios
-    .get('/ajax/favorites')
-    .then(response => {
-      this.page = response.data.pageResult
-      this.paginationBar = response.data.paginationBar
-    })
-    .catch(error => {
-      console.log(error)
-    })
+    this.loadFavorites()
   },
   methods: {
-    isTabActive: function (tab) {
-      console.log(tab +" -- "+ this.page.paginationCriteria.sortingCriteria)
-      var active = this.page ? this.page.paginationCriteria.sortingCriteria == tab : false
+    selectSortCriteria: function(criteria){
+        this.loadFavorites(criteria)
+    },
+    selectSortDirection: function(dir){
+        this.loadFavorites(this.page.paginationCriteria.sortingCriteria, dir)
+    },
+
+    //
+    loadFavorites: function(sortBy, sortDir){
+     axios
+        .get('/ajax/favorites', {
+           params: {
+             sortBy: sortBy || 'Activity',
+             sortDir: sortDir || 'Desc'
+           }
+        })
+        .then(response => {
+          this.page = response.data.pageResult
+          this.paginationBar = response.data.paginationBar
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    isSortCriteriaActive: function (criteria) {
+      var active = this.page ? this.page.paginationCriteria.sortingCriteria == criteria : false
       return {"has-text-weight-semibold" : active }
     },
-    isSortDirActive: function(dir) {
-      console.log(dir +" -- "+ this.page.paginationCriteria.sortingDirection)
+    isSortDirectionActive: function(dir) {
       var active = this.page ? this.page.paginationCriteria.sortingDirection == dir : false
       return { "is-info" : active, "is-selected" : active}
     },
