3343d7f5f9006afa123fa47d3ae127580e864bf0
David Castelletti <castellettid@gmail.com>
2021-01-04 15:04:27 +0100

    Remove GA from backend code

--- src/main/kotlin/com/davioooh/qsearch/handlers/RootHandler.kt
+++ src/main/kotlin/com/davioooh/qsearch/handlers/RootHandler.kt
@@ -1,11 +1,10 @@
 package com.davioooh.qsearch.handlers
 
-import com.davioooh.qsearch.config.GA_CODE
 import io.javalin.http.Context
 import io.javalin.http.Handler
 
 class RootHandler : Handler {
     override fun handle(ctx: Context) {
-        ctx.render("/templates/index.html", mapOf("gaCode" to GA_CODE))
+        ctx.render("/templates/index.html")
     }
 }
